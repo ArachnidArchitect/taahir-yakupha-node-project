16abc8baf09ab4a2659fa70c3e988bf3785f525d
WanderingWanderer <proffesionalworkingstuff@gmail.com>
2024-08-22 07:11:48 +0200

    changed the index.js to allow jwt headers and a static files

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const app = express()
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:'http://localhost:8080',
+    credentials:true
+}))
+app.use(express.static('public'))
 app.use('/products', productsRoute)
 app.use('/users', usersRoute)
 
